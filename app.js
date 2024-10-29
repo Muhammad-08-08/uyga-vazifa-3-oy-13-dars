c6dc5d8f4fe2dcdd22066fb074496870ca3159c9
muhammad <muhammadnuraliyev119@gmail.com>
2024-10-29 08:48:35 +0500

    muhammad

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
 let data = [
   {
-    id:1,
+    id: 1,
     name: "John Doe",
     age: 30,
     email: "john@example.com",
     phone: "123-456-7890",
   },
   {
-    id:2,
+    id: 2,
     name: "Jane Smith",
     age: 25,
     email: "jane@example.com",
     phone: "987-654-3210",
   },
   {
-    id:3,
+    id: 3,
     name: "Tom Hardy",
     age: 40,
     email: "tom@example.com",
     phone: "765-432-1098",
   },
   {
-    id:4,
+    id: 4,
     name: "Emily Blunt",
     age: 28,
     email: "emily@example.com",
     phone: "321-654-9870",
   },
   {
-    id:5,
+    id: 5,
     name: "Emily Blunt",
     age: 28,
     email: "emily@example.com",
@@ -56,20 +56,19 @@ let data = [
 
 // function reyt(email){
 //   return data.find((val)=>val.email.toLocaleLowerCase().includes(email.toLocaleLowerCase()));
-  
+
 // }
 
 // console.log(reyt("emily@example.com"))
 
 //? 2-misol tugadi
- 
 
 // ! 3-misol
 
 // function Update(id, arr){
 //   let index = data.findIndex((val)=>
 //   val.id= id)
-//   if(index== -1) return 
+//   if(index== -1) return
 //   data[index]={
 //     ...data[index],
 //     ...arr,
@@ -82,11 +81,11 @@ let data = [
 
 // ! 4-misol
 
-function deleteo(id){
-  let filter =data.filter((val)=>val.id !==id) 
-  return filter
+function deleteo(id) {
+  let filter = data.filter((val) => val.id !== id);
+  return filter;
 }
-console.log(deleteo(1))
+console.log(deleteo(1));
 
 // ! 4-misol tugadi
 
